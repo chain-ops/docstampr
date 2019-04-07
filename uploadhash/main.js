72a19b9640d7bc0e5de4e88febda82f86fa26cfa
Adrien Poupard <adrien.poupard@gmail.com>
2019-04-07 12:53:50 +0200

    Only handle one file

--- uploadhash/main.js
+++ uploadhash/main.js
@@ -13,45 +13,44 @@ App.sendHash = function (hash) {
     }, 'text');
 }
 
+App.template = function (files, fileIndex) {
+                return `<div class="file file--${fileIndex}">
+                    <div class="name"><span>${files[fileIndex].name}</span></div>
+                    <div class="progress active"></div>
+                    <div class="done">
+	                    <a href="" target="_blank">
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 1000 1000">
+		                        <g><path id="path" d="M500,10C229.4,10,10,229.4,10,500c0,270.6,219.4,490,490,490c270.6,0,490-219.4,490-490C990,229.4,770.6,10,500,10z M500,967.7C241.7,967.7,32.3,758.3,32.3,500C32.3,241.7,241.7,32.3,500,32.3c258.3,0,467.7,209.4,467.7,467.7C967.7,758.3,758.3,967.7,500,967.7z M748.4,325L448,623.1L301.6,477.9c-4.4-4.3-11.4-4.3-15.8,0c-4.4,4.3-4.4,11.3,0,15.6l151.2,150c0.5,1.3,1.4,2.6,2.5,3.7c4.4,4.3,11.4,4.3,15.8,0l308.9-306.5c4.4-4.3,4.4-11.3,0-15.6C759.8,320.7,752.7,320.7,748.4,325z"</g>
+		                    </svg>
+						</a>
+                    </div>
+                </div>
+                <div id="file-input-hash" class="hash"></div>`;
+}
 App.init = (function() {
     const $ = document.querySelector.bind(document);
-    //Init
-    function handleFileSelect(evt) {
-        const files = evt.target.files; // FileList object
 
+    function handleFileSelectEvent(evt) {
+        const files = evt.target.files;
+        handleFileSelect(files)// FileList object
+    }
+
+    function handleFileSelect(files) {
         //files template
-        let template = `${Object.keys(files)
-            .map(file => `<div class="file file--${file}">
-     <div class="name"><span>${files[file].name}</span></div>
-     <div class="progress active"></div>
-     <div class="done">
-	<a href="" target="_blank">
-      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 1000 1000">
-		<g><path id="path" d="M500,10C229.4,10,10,229.4,10,500c0,270.6,219.4,490,490,490c270.6,0,490-219.4,490-490C990,229.4,770.6,10,500,10z M500,967.7C241.7,967.7,32.3,758.3,32.3,500C32.3,241.7,241.7,32.3,500,32.3c258.3,0,467.7,209.4,467.7,467.7C967.7,758.3,758.3,967.7,500,967.7z M748.4,325L448,623.1L301.6,477.9c-4.4-4.3-11.4-4.3-15.8,0c-4.4,4.3-4.4,11.3,0,15.6l151.2,150c0.5,1.3,1.4,2.6,2.5,3.7c4.4,4.3,11.4,4.3,15.8,0l308.9-306.5c4.4-4.3,4.4-11.3,0-15.6C759.8,320.7,752.7,320.7,748.4,325z"</g>
-		</svg>
-						</a>
-     </div>
-    </div>`)
-            .join("")}`;
-
+        let file = 0;
         $("#drop").classList.add("hidden");
         $("footer").classList.add("hasFiles");
+        $(".importar").classList.add("active");
         setTimeout(() => {
-            $(".list-files").innerHTML = template;
+            $(".list-files").innerHTML = App.template(files, file);
         }, 1000);
 
-        Object.keys(files).forEach(file => {
-            let load = 2000 + (file * 2000); // fake load
-            setTimeout(() => {
-                $(`.file--${file}`).querySelector(".progress").classList.remove("active");
-                $(`.file--${file}`).querySelector(".done").classList.add("anim");
-            }, load);
-        });
-    }
-    function sha256Hash(file) {
-        var reader = new FileReader();
-        reader.onload = onFileReady;
-        reader.readAsBinaryString(file);
+        let load = 2000; // fake load
+        setTimeout(() => {
+            $(`.file--${file}`).querySelector(".progress").classList.remove("active");
+            $(`.file--${file}`).querySelector(".done").classList.add("anim");
+            sha256Hash(files[file]);
+        }, load);
     }
 
     // trigger input
@@ -71,11 +70,15 @@ App.init = (function() {
     };
     $("#drop").ondrop = evt => {
         $("input[type=file]").files = evt.dataTransfer.files;
-        sha256Hash(evt.dataTransfer.files[0]);
-        $("footer").classList.add("hasFiles");
-        $("#drop").classList.remove("active");
+        handleFileSelect(evt.dataTransfer.files);
         evt.preventDefault();
     };
+
+    function sha256Hash(file) {
+        var reader = new FileReader();
+        reader.onload = onFileReady;
+        reader.readAsBinaryString(file);
+    }
 
     function onFileReady(event) {
         var hash = sha256(event.target.result);
@@ -83,11 +86,9 @@ App.init = (function() {
         showHash(hash);
     }
 
-
     function showHash(hash) {
         $("#file-input-hash").innerText = hash;
         hide($("#drop"));
-        show($("#hash"));
     }
 
     function hide(elem) {
@@ -97,6 +98,15 @@ App.init = (function() {
         elem.classList.remove("hidden");
     }
 
+    $(".importar").addEventListener("click", () => {
+        $(".list-files").innerHTML = "";
+        $("footer").classList.remove("hasFiles");
+        $(".importar").classList.remove("active");
+        setTimeout(() => {
+            $("#drop").classList.remove("hidden");
+        }, 500);
+    });
+
     // input change
-    $("input[type=file]").addEventListener("change", handleFileSelect);
+    $("input[type=file]").addEventListener("change", handleFileSelectEvent);
 })();
